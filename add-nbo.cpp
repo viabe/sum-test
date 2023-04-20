8bb2ee908d84a0a0e7f619470f9516dac506fa6a
badakim <skqldhkd123@naver.com>
2023-04-20 19:57:04 +0900

    add add-nbo.cpp

---
+++ add-nbo.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include<string.h>
+#include <stdint.h>
+#include <WinSock2.h>
+
+void usage() {
+	printf("syntax : add-nbo <file1> <file2>\n");
+	printf("sample : add-nbo a.bin c.bin");
+}
+
+uint32_t readInt(char* fileName) {
+	FILE* fp = fopen(fileName, "rb");
+	if (fp == nullptr) {
+		fprintf(stderr, "fopen returns null %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	uint32_t n;
+	size_t readLen = fread(&n, 1, sizeof(n), fp);
+	if (readLen != sizeof(n)) {
+		fprintf(stderr, "fread return %lu\n", readLen);
+		exit(EXIT_FAILURE);
+	}
+	n = ntohl(n);
+	fclose(fp);
+	return n;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		usage();
+		exit(EXIT_FAILURE);
+	}
+
+	uint32_t n1 = readInt(argv[1]);
+	uint32_t n2 = readInt(argv[2]);
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", n1, n1, n2, n2, n1 + n2, n1 + n2);
+}
+
